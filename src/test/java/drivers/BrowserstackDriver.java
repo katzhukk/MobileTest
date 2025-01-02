73842aa5f7856e3f07ddac749a6b6b913356dec1
katzhukk <katzhukova230@gmail.com>
2025-01-02 13:32:53 +0300

    added mobile test

---
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -0,0 +1,53 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import config.WebDriverConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackDriver implements WebDriverProvider {
+
+    private final WebDriverConfig config;
+
+    public BrowserstackDriver() {
+        this.config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+    }
+
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities caps = new MutableCapabilities();
+
+        // Set your access credentials
+        caps.setCapability("browserstack.user", config.getBrowserstackUser());
+        caps.setCapability("browserstack.key", config.getBrowserstackKey());
+
+        // Set URL of the application under test
+        caps.setCapability("app", config.getApp());
+
+        // Specify device and os_version for testing
+        caps.setCapability("device", config.getDevice());
+        caps.setCapability("os_version", config.getOsVersion());
+
+        // Set other BrowserStack capabilities
+    //    caps.setCapability("project", "First Java Project");
+    //    caps.setCapability("build", "browserstack-build-1");
+        //   caps.setCapability("name", "first_test");
+
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
+        try {
+            return new RemoteWebDriver(
+                    new URL(config.getRemoteUrl()), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
